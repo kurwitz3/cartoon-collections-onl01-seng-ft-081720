b0144207d251270fa078b29dc13da681510081cc
Richard Kurwitz <kurwitz3@aol.com>
2020-08-20 01:06:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |name, number|
+    puts "#{number} #{name}" 
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+    new_array = []
+    array.collect {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def long_planeteer_calls(array)
+     new_array =[]
+     array.each do |x|
+       new_array << x.count
+     
+      z = new_array.find{|item| item.include?(5)}
+  end
 end
